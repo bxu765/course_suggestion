5614985c330122dc92646b399a4f7fddaf26184b
Brian Xu <jqt7xn@virginia.edu>
2026-03-30 21:30:09 -0400

    api documentation added + example template

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # course_suggestion
+
+## api calls
+
+use sis and hooslist api. example in `example_get.py`. sis api is very slow, hooslist is slightly better.
+
+### sis api
+sis api documentation: https://s23.cs3240.org/sis-api.html
+
+get all course mnemonics:
+```
+https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term={term}
+```
+
+* replace `{term}` with `1268` for spring 2026
+
+get all courses given subject:
+```
+https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term={term}&subject={subject}&page={page}
+```
+
+* replace `{term}` as before
+* replace `{subject}` with course mnemonic
+* replace `{page}` with page number. if `0`, then `response['pageCount']` stores how many pages, otherwise it is `0`.
+
+### hooslist api
+no documentation for this one, i found it through dev tools and looking at all network calls when using hooslist webpage.
+
+get course description:
+```
+https://hooslist.virginia.edu/ClassSchedule/_GetCourseDescription?subject={subject}&courseNum={number}
+```
+
+* replace `{subject}` with course mnemonic
+* replace `{number}` with course number
---
+++ example_get.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+# endpoint for retrieving and parsing subjects
+subj_query="https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearchOptions?institution=UVA01&term=1268"
+subjects=[s['subject'] for s in requests.get(subj_query).json()['subjects']]
+
+# endpoint for searching through each subject
+# sis api is partially documented here: https://s23.cs3240.org/sis-api.html
+# hooslist api has no documentation, i had to find it through capturing the network calls of hooslist using dev tools
+num_template='https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01&term=1268&subject='
+desc_template='https://hooslist.virginia.edu/ClassSchedule/_GetCourseDescription?subject='
+
+# FOR TESTING
+subjects=['CS']
+
+# local storage of descs, probably redundant now i found the hooslist endpoint that works
+out={}
+for subject in subjects:
+    print(subject)
+    query=num_template+subject+'&page='
+    pageCount=requests.get(query+'1').json()['pageCount']
+    subj_ids={}
+    for page in range(pageCount):
+        response=requests.get(query+f'{page+1}').json()['classes']
+        for course in response:
+            if course['catalog_nbr'] not in subj_ids:
+                print(course['catalog_nbr'])
+                desc_query=desc_template+subject+'&courseNum='+course['catalog_nbr']
+                desc_response=requests.get(desc_query).text
+                subj_ids[course['catalog_nbr']]=desc_response
+    out[subject]=subj_ids
+with open('desc.json','w') as f: json.dump(out,f,indent=4)
